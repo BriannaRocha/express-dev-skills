2fe4ca535544654c3c2d0c1bcc6187e152d84762
BriannaRocha <rochabrianna17@gmail.com>
2024-01-16 03:07:07 -0500

    add controllers for skills

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,11 +1,11 @@
 const skills = [
     {text: 'JavaScript', mastered: true, _id: 125223},
-    {text: 'CSS', mastered: false, _id: 127904},
-    {text: 'HTML', mastered: false, _id: 139608},
+    {text: 'CSS', mastered: true, _id: 127904},
+    {text: 'HTML', mastered: true, _id: 139608},
     {text: 'Node', mastered: false, _id: 145382},
     {text: 'MongoDB', mastered: false, _id: 117343},
-  ]
-  
-  export { 
-      skills
-  }
+]
+
+export { 
+    skills
+}
