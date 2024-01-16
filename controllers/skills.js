f2f24ef6d6119558dd80467872cf1c44c0d201d9
BriannaRocha <rochabrianna17@gmail.com>
2024-01-16 04:24:27 -0500

    refactor the index controller

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,11 +1,18 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
+  Skill.find({})
+  .then(skills => { 
     res.render('skills/index', {
-      skills: skills
+      skills: skills,
     })
+  })
+  .catch(error => { 
+    console.log(error)
+    res.redirect('/')
+  })
 }
 
 export {
-	index,
+  index
 }
